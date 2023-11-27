0d0d9e05593d07b8a89feaf69a5fa8edd2eafb86
Md Saifur Rahman <saifurrshishir@gmail.com>
2023-11-27 17:54:12 +0600

    feat(api): create an endpoint for users
    Admin also can use this api to turn someone into a new admin

--- index.js
+++ index.js
@@ -10,10 +10,7 @@ const port = process.env.PORT || 5000;
 //middlewares
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5174"
-    ],
+    origin: ["http://localhost:5173", "http://localhost:5174"],
     credentials: true,
   })
 );
@@ -37,20 +34,6 @@ const verifyToken = (req, res, next) => {
   });
 };
 
-
-const verifyTeacher = async (req, res, next) => {
-  const email = req.decoded.email;
-  console.log("teacher email:", email);
-  const query = { email: email };
-  const user = await userCollection.findOne(query);
-  const isAdmin = user?.role === "teacher";
-  if (!isAdmin) {
-    return res.status(403).send({ message: "access forbidden" });
-  }
-  next();
-};
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ddl1jzo.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -70,20 +53,6 @@ async function run() {
     const userCollection = database.collection("users");
     const teacherRequestCollection = database.collection("teacherRequests");
 
-    // middlewares
-    const verifyAdmin = async (req, res, next) => {
-      const email = req.decoded.email;
-      console.log("admin email:", email);
-      const query = { email: email };
-      const user = await userCollection.findOne(query);
-      const isAdmin = user?.role === "admin";
-      if (!isAdmin) {
-        return res.status(403).send({ message: "access forbidden" });
-      }
-      next();
-    };    
-
-
     // auth related endpoints
     app.post("/api/v1/auth/access-token", (req, res) => {
       const user = req.body;
@@ -100,26 +69,30 @@ async function run() {
         .send({ success: true });
     });
 
-
-    app.post("/api/v1/auth/logout", (req, res) => {
-      // console.log("Logout request received");
-      res.clearCookie("token").send({ success: true });
-    });
-
-
-    // user related api
-    app.get("/api/v1/users", verifyToken, async (req, res) => {
-      const queryEmail = req.query.email;
-      // console.log("req--->",req)
-      // console.log("query email from api--->",queryEmail)
-      if (queryEmail !== req.decoded.email) {
-        return res.status(403).send({ message: "Forbidden" });
+    // middlewares
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      // console.log("admin email:", email);
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "access forbidden" });
       }
-      const query = { email: queryEmail };
-      const user = await userCollection.findOne(query);    
-      res.send({user});
-    });  
+      next();
+    };
 
+    const verifyTeacher = async (req, res, next) => {
+      const email = req.decoded.email;
+      // console.log("teacher email:", email);
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === "teacher";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "access forbidden" });
+      }
+      next();
+    };
 
     app.get("/api/v1/users/admin/:email", verifyToken, async (req, res) => {
       const paramEmail = req.params.email;
@@ -135,7 +108,52 @@ async function run() {
       res.send({ admin });
     });
 
+    // user related api
+    app.get("/api/v1/users", verifyToken, verifyAdmin, async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
 
+    app.get("/api/v1/user", verifyToken, async (req, res) => {
+      const queryEmail = req.query.email;
+      // console.log("req--->",req)
+      // console.log("query email from api--->",queryEmail)
+      if (queryEmail !== req.decoded.email) {
+        return res.status(403).send({ message: "Forbidden" });
+      }
+      const query = { email: queryEmail };
+      const user = await userCollection.findOne(query);
+      res.send({ user });
+    });
+
+    app.post("/api/v1/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists", insertedId: null });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.patch(
+      "/api/v1/users/make-admin/:id",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const userId = req.params.id;
+        const filter = { _id: new ObjectId(userId) };
+        const updateDoc = {
+          $set: { role: "admin" },
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+
+        res.send({ success: true, message: "User is now an admin" });
+      }
+    );
+
+    // teacher request related endpoints
     app.get("/api/v1/users/teacher/:email", verifyToken, async (req, res) => {
       const paramEmail = req.params.email;
       if (paramEmail !== req.decoded.email) {
@@ -150,63 +168,42 @@ async function run() {
       res.send({ teacher });
     });
 
-
-    app.post("/api/v1/users", async (req, res) => {
-      const user = req.body;
-      const query = { email: user.email };
-      const existingUser = await userCollection.findOne(query);
-      if (existingUser) {
-        return res.send({ message: "user already exists", insertedId: null });
+    app.get(
+      "/api/v1/users/teacher-requests",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const result = await teacherRequestCollection.find().toArray();
+        res.send(result);
       }
-      const result = await userCollection.insertOne(user);
-      res.send(result);
-    });
+    );
 
-
-    // teacher request related endpoints
-    app.get("/api/v1/users/teacher-requests", verifyToken, verifyAdmin, async (req, res)=>{     
-      const result = await teacherRequestCollection.find().toArray();
-      res.send(result);
-    })
-
-
-    app.get("/api/v1/users/my-teaching-requests/:email", verifyToken, async (req, res)=>{ 
-      const paramsEmail = req.params.email;
-      // console.log("params Email",paramsEmail)      
-      const tokenEmailFromVerifyToken = req.decoded.email; 
-      // console.log("token Email",tokenEmailFromVerifyToken)      
-      if (paramsEmail !== tokenEmailFromVerifyToken) {
-        return res.status(403).send({ message: "403, Access forbidden" });
+    app.get(
+      "/api/v1/users/my-teaching-requests/:email",
+      verifyToken,
+      async (req, res) => {
+        const paramsEmail = req.params.email;
+        // console.log("params Email",paramsEmail)
+        const tokenEmailFromVerifyToken = req.decoded.email;
+        // console.log("token Email",tokenEmailFromVerifyToken)
+        if (paramsEmail !== tokenEmailFromVerifyToken) {
+          return res.status(403).send({ message: "403, Access forbidden" });
+        }
+        const query = { email: paramsEmail };
+        const result = await teacherRequestCollection.findOne(query);
+        res.send(result);
       }
-      const query = {email: paramsEmail};    
-      const result = await teacherRequestCollection.findOne(query);
-      res.send(result);
-    })
+    );
 
-
-    app.post("/api/v1/users/teacher-requests", verifyToken, async (req, res)=>{
-      const teacherReq = req.body;
-      const result = await teacherRequestCollection.insertOne(teacherReq);
-      res.send(result);
-    })
-
-
-    app.patch("/api/v1/users/teacher-requests", verifyToken, async (req, res)=>{
-      const teacherReq = req.body;
-      const result = await teacherRequestCollection.insertOne(teacherReq);
-      res.send(result);
-    })
-
-  
-
-
-
-
-
-
-
-
-    
+    app.post(
+      "/api/v1/users/teacher-requests",
+      verifyToken,
+      async (req, res) => {
+        const teacherReq = req.body;
+        const result = await teacherRequestCollection.insertOne(teacherReq);
+        res.send(result);
+      }
+    );
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
